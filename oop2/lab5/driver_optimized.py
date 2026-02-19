ccaa3234c2c84af35642ec594e12f4afe31ef01a
cueki <github2@madisonlovett.dev>
2026-02-19 10:51:05 -0800

    doc clarification

--- oop2/lab5/driver_optimized.py
+++ oop2/lab5/driver_optimized.py
@@ -1,7 +1,7 @@
 """
 I use str.translate (very underutilized if I do say so myself) to make a lookup table so that translate() is O(n).
 
-Then open the file in binary (for obvious reasons). Then do said translate() to process the whole file in a single call.
+Then open the whole file in binary to skip any automatic python text parsing. Then do said translate() to process the whole file in a single call.
 
 The real optimization begins when I split()[::-1] to reverse the input order, such that the first occurrence in the original order is kept,
 then call lower() on the same giant string to create keys for a dict(zip(keys, values)).
@@ -11,6 +11,8 @@ Due to how dicts work, this mapping of lowercase keys to original case values ca
 I then handle the case where split() after translate() drops any tokens that become empty strings after punctuation removal by adding an empty string.
 
 Then I simply build the list with the dict values. The print call is also using unpacking to do it all in a single operation.
+
+Total function call count from cProfiles: 16.
 """
 
 
